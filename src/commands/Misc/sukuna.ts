2458a9b302d1896ebcc7e081084a45afe01f00af
well300 <90952986+well300@users.noreply.github.com>
2021-09-28 19:59:24 +0530

    Update sukuna.ts

--- src/commands/Misc/sukuna.ts
+++ src/commands/Misc/sukuna.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
         const n = [
             './assets/videos/Sukuna/sukuna-git.mp4'
         ]
-        let chitoge = n[Math.floor(Math.random() * n.length)]
+        let sukuna = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: sukuna }, MessageType.video, {
             mimetype: Mimetype.gif,
             caption: `火 ʂυƙυɳα\n\n⚜ *Description: Contact owner for repo *<!mod/>* \n\n\n` }
