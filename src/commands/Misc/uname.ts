ea0767ece5d257da2b12b520f65279ec869b9c28
Lord Rakesh <89836389+lordrakesh@users.noreply.github.com>
2021-10-31 21:44:51 +0530

    🎃

--- src/commands/Misc/uname.ts
+++ src/commands/Misc/uname.ts
@@ -15,6 +15,290 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void (await M.reply(`👒 Your user name is ${M.sender.username}!`))
+        return void (await M.reply(`OWNER
+
+🎗 #off
+
+🎗 #on
+
+🎗 #status
+
+🎗 #setthumb
+
+🎗 #settarget
+
+🎗 #setfakeimg
+
+🎗 #setreply
+
+🎗 #setprefix [2 Button]
+
+🎗 #mode [2 Button self/public]
+
+🎗 #term <code>
+
+🎗 #eval <code>
+
+🎗 #colongsw [reply sw]
+
+    
+
+GRUP
+
+🧧 #grup [3 Button]
+
+🧧 #promote <reply chat member>
+
+🧧 #demote <reply chat member>
+
+🧧 #setdesc
+
+🧧 #setname
+
+🧧 #kick <reply/tag member>
+
+🧧 #add <reply/tag member>
+
+🧧 #getbio <reply chat member>
+
+🧧 #getname <reply chat member>
+
+🧧 #reminder <msg/2s>
+
+🧧 #listonline
+
+🧧 #linkgc
+
+🧧 #sider [reply chat bot]
+
+🧧 #antilink
+
+🧧 #antihidetag
+
+🧧 #antiviewonce
+
+🧧 #antivirus
+
+🧧 #kickarea <anti +1 number>
+
+🧧 #leveling <1(on)/0(off)>
+
+🧧 #delete <reply to bot msg>
+
+🧧 #bc <broadcast both>
+
+🧧 #bcgc <broadcast grup>
+
+🧧 #profile
+
+    
+
+MAKER
+
+🎭 #wolf <text>
+
+🎭 #tfire <text>
+
+🎭 #ytgold <text>
+
+🎭 #ytsilver <text>
+
+🎭 #t3d  <text>
+
+🎭 #logoa <text&text>
+
+🎭 #phlogo <text&text>
+
+🎭 #write <text>
+
+🎭 #phcomment <text&text>
+
+🎭 #marvel <text&text>
+
+🎭 #leavest<text>
+
+🎭 #neon <text>
+
+🎭 #wall <text>
+
+🎭 #sticker
+
+🎭 #swm <author|packname>
+
+🎭 #take <author|packname>
+
+🎭 #fdeface
+
+🎭 #emoji
+
+    
+
+CONVERT
+
+🧩 #toimg
+
+🧩 #tomp3
+
+🧩 #tomp4
+
+🧩 #slow
+
+🧩 #fast
+
+🧩 #reverse
+
+🧩 #tourl
+
+🧩 #tts
+
+🧩 #attp
+
+    
+
+DOWNLOADER
+
+🎼 #youtube <url>
+
+🎼 #tiktok <url>
+
+🎼 #instagram <url>
+
+🎼 #twitter <url>
+
+UP STORY
+
+🎆 #upswtxts
+
+🎆 #upswimage
+
+🎆 #upswvideo
+
+    
+
+CONTACT
+
+🎍 #slander
+
+🎍 #slanderpc
+
+🎍 #contact
+
+    
+
+TAG
+
+🔗 #tagall
+
+🔗 #hidetag
+
+🔗 #contacttag
+
+🔗 #sticktag
+
+🔗 #mediatag
+
+FUN
+
+✨ #truth
+
+✨ #dare
+
+✨ #lesbiancheck
+
+✨ #handsomecheck
+
+✨ #gaycheck
+
+✨ #beautifulcheck
+
+✨ #uglycheck
+
+✨ #uglycheck
+
+✨ #slap
+
+    
+
+OTHER
+
+🎨 #ping
+
+🎨 #inspect
+
+🎨 #join
+
+🎨 #searchmessage <query>
+
+🎨 #get
+
+🎨 #ytsearch <query>
+
+🎨 #igstalk <query>
+
+🎨 #githubstalk <query>
+
+🎨 #tiktokstalk <query>
+
+🎨 #play <query>
+
+🎨 #video <query>
+
+🎨 #igstory <username>
+
+🎨 #twitter <link>
+
+🎨 #tiktok <link>
+
+🎨 #tiktokaudio <link>
+
+🎨 #fb <link>
+
+🎨 #brainly <query>
+
+🎨 #image <query>
+
+🎨 #anime <random>
+
+🎨 #pinterest <query>
+
+🎨 #comic <query>
+
+🎨 #lyrics <query>
+
+🎨 #chara <query>
+
+🎨 #playstore <query>
+
+🎨 #otaku <query>
+
+    
+
+SIMPLE BOT
+
+🔧 #jadibot
+
+🔧 #stopjadibot
+
+🔧 #listbot
+
+STICKER CMD
+
+⚙ #addcmd
+
+⚙ #delcmd
+
+⚙ #listcmd
+
+    
+
+VOTE
+
+♦ #voting
+
+♦ #delvote
+
+♦ #vote
+
+♦ #devote`))
     }
 }
