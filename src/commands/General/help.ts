f1bd92ba309a090ad671ab83630dc7b1dd24a3d4
Lord Rakesh <89836389+lordrakesh@users.noreply.github.com>
2021-11-01 15:03:48 +0530

    Update help.ts

--- src/commands/General/help.ts
+++ src/commands/General/help.ts
@@ -1,67 +1,80 @@
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'help',
-            description: 'Generally used to check if bot is Up',
+            description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
-            usage: `${client.config.prefix}help`,
-            baseXp: 0
+            usage: `${client.config.prefix}help (command_name)`,
+            aliases: ['h']
         })
     }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void (await M.reply(`Konnichiwa! *${M.sender.username}*, *I'm Sukuna!* I'm ready for help.
+    run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
+            const n = [
+            './assets/videos/Sukuna/sukuna.mp4'
+        ]
+        let sukuna = n[Math.floor(Math.random() * n.length)]
+        if (!parsedArgs.joined) {
+            const commands = this.handler.commands.keys()
+            const categories: { [key: string]: ICommand[] } = {}
+            for (const command of commands) {
+                const info = this.handler.commands.get(command)
+                if (!command) continue
+                if (!info?.config?.category || info.config.category === 'dev') continue
+                if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
+                else {
+                    categories[info.config.category] = []
+                    categories[info.config.category].push(info)
+                }
+            }
+            let text = `
+Konichiwa! ${M.sender.username}, I'm Sukuna , Know Your Place Fool 太陽
 
-My perfix - !
-
-- 🄶🄴🄽🄴🅁🄰🄻
-
-◍!google
-◍!covid
-◍!github
-◍!profile
-◍!hi
-◍!owner
-◍!support
-◍!xp
-◍!weather
-◍!sukuna
-◍!bot/chat
-
-- 🄼🄴🄳🄸🄰
-
-◍ !sticker
-◍ !toimg
-◍ !play
-◍ !ytmp3
-◍ !ytmp4
-◍ !yts
-◍ !fb
-◍ !ig
-◍ !subreddit
-
- *- 🄰🄳🄼🄸🄽* 
-
-◍ !add
-◍ !kick
-◍ !promote
-◍ !demote
-◍ !enable antilink
-◍ !tagall
-◍ !hidetag
-◍ !linkgroup
-
-- 🅆🄴🄴🄱
-
-◍ !waifu
-◍ !neko
-◍ !wallpaperanime
-
+My Pefix - /                 \n\n`
+            const keys = Object.keys(categories)
+            for (const key of keys)
+                text += `❖┈ᕮ✧${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}✧ᕭ┈❖\n• \`\`\`${categories[
+                    key
+                ]
+                    .map((command) => command.config?.command)
+                    .join('\n ')}\`\`\`\n\n`
+            return void this.client.sendMessage(M.from, { url: sukuna }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `${text} 
+ 
 Sooooo, My work is done. Bye! 
+Don't Summon Me Again (๑•﹏•)
 
-Any problem? Remember me :)
+
+火┈[𝐆𝐚𝐦𝐛𝐚𝐫𝐞 𝐆𝐚𝐦𝐛𝐚𝐫𝐞]┈火
+
+🪵 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
+            )
+        }
+        const key = parsedArgs.joined.toLowerCase()
+        const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
+        if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
+        const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
+        M.reply(
+            `🚀 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
+                state ? 'Disabled' : 'Available'
+            }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
+                command.config.aliases
+                    ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
+                    : ''
+            }\n🎐 *Group Only:* ${this.client.util.capitalize(
+                JSON.stringify(!command.config.dm ?? true)
+            )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
+        )
+    }
+
+    emojis = ['🈷️', '🈹️', '🈯️', '☮', '🈳️', '🈵️', '🈂️', '㊗️', '🈁️']
+}
